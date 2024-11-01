ce86ab1896d5ba362f7bb6b60978c9191c92767f
akeenan360 <akeenan360@gmail.com>
2024-11-01 02:45:06 -0400

    removed ItemMap from locations that don't contain the string "store"

--- Location.java
+++ Location.java
@@ -4,14 +4,17 @@ import java.util.Map;
 public class Location {
     protected String Name;
     // map of items with item and quantity
-    protected Map<Item, Integer> ItemMap = new LinkedHashMap<>();
+    protected Map<Item, Integer> ItemMap;
 
     Location(String name) {
-        InitItemMap();
+        if (name.toLowerCase().contains("store")) {
+            InitItemMap();
+        }
         this.Name = name;
     }
 
     private void InitItemMap() {
+        this.ItemMap = new LinkedHashMap<>();
         ItemMap.put(new Item("Dirt", 0.1), 10_000);
         ItemMap.put(new Item("Sand", 1.0), 1_000);
         ItemMap.put(new Item("Wood", 2.0), 100);
